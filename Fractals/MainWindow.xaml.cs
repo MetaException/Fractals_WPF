558ff2cd9667e9d9c9167fbabb1d020fabd00623
MetaException <0x0_email_address_not_found@mail.ru>
2022-09-13 19:39:45 +0300

    initial commit

---
+++ Fractals/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Fractals
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
