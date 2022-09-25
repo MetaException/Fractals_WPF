793955e6e619ceb680dc5f011911ea6c101763d0
MetaException <0x0_email_address_not_found@mail.ru>
2022-09-25 14:37:42 +0300

    Rewrite to use GPU
    * Use ComputeSharp(https://github.com/Sergio0694/ComputeSharp) library run C# code in parallel on the GPU
    * Update Visual Studio and the project

---
+++ Fractal/GPU.cs
@@ -0,0 +1,44 @@
+﻿using ComputeSharp;
+
+[AutoConstructor]
+public readonly partial struct DrawMandelbrotSet : IComputeShader
+{
+    public readonly IReadWriteNormalizedTexture2D<float4> texture;
+
+    public void Execute()
+    {
+        //a = x / масштаб * 1.777f(константа 16:9 для правильной отрисовки)
+        float c_real = (float)(ThreadIds.X - (texture.Width / 2.0f)) / (float)(texture.Width / 4.0f) * 1.777f;
+
+        //a = y / масштаб
+        float c_imaginary = (float)(ThreadIds.Y - (texture.Height / 2.0f)) / (float)(texture.Height / 4.0f);
+
+        float z_real = 0.0f; 
+        float z_imaginary = 0.0f;
+
+        int iterations = 0;
+        do
+        {
+            iterations++;
+            float z_real2 = (z_real * z_real) - (z_imaginary * z_imaginary) + c_real;
+            float z_imaginary2 = (z_real * z_imaginary * 2) + c_imaginary;
+            //z = z * z + c; // z = z ^ complexPower + c
+
+            if (z_real2 * z_real2 + z_imaginary2 * z_imaginary2 >= 4.0f)
+            {
+                texture[ThreadIds.XY].RGB = 0 + (float)iterations / 64f;
+                break;
+            }
+            if (iterations < 2)
+            {
+                float P = Hlsl.Sqrt(Hlsl.Pow(z_real2 - 1f / 4f, 2f) + z_imaginary2 * z_imaginary2);
+                float O = Hlsl.Atan2(z_imaginary2, z_real2 - 1f / 4f);
+                float Pc = 1f / 2f - 1f / 2f * Hlsl.Cos(O);
+                if (P <= Pc)
+                    break;
+            }
+            z_real = z_real2;
+            z_imaginary = z_imaginary2;
+        } while (iterations < 100);
+    }
+}
---
+++ Fractal/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ComputeSharp;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Fractal
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+            // Load a texture from a specified image, and decode it in the BGRA32 format
+            using var texture = GraphicsDevice.GetDefault().AllocateReadWriteTexture2D<Bgra32, float4>((int)Width, (int)Height);
+
+            // Run our shader on the texture we just loaded
+            GraphicsDevice.GetDefault().For(texture.Width, texture.Height, new DrawMandelbrotSet(texture));
+
+            // Save the processed image by overwriting the original image
+            texture.Save("D:\\myImage.jpg");
+
+            img.Source = new BitmapImage(new Uri("D:\\myImage.jpg")); //Переделать
+        }
+    }
+}
